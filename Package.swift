709a9e90c4adcb357bafee6944debcb453e3f7bd
Tarik ALAOUI M'HAMDI <71636934+talaoui-accor@users.noreply.github.com>
2024-01-04 16:14:50 +0100

    update version

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "SwiftSyntax",
-                 url: "https://github.com/apple/swift-syntax.git",
-                 .exact("508.0.0")),
+                 url: "https://github.com/talaoui-accor/swift-syntax"
+                 ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
