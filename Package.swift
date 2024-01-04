c6bb0039c68900801a3d5540af4d4d947b9a4360
Tarik ALAOUI M'HAMDI <71636934+talaoui-accor@users.noreply.github.com>
2024-01-04 17:37:23 +0100

    update version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftSemantics",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
